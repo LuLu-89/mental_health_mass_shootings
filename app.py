bf4e3982c3740e4601c6b76716fe3673057ec8b3
Brian Hackett <brianrhackett83@gmail.com>
2019-06-29 13:00:32 -0600

    flask skeleton

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, render_template, redirect
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+import pandas as pd
+import config
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+engine = create_engine(
+    f'sqlite:///resources/tstwp.sqlite',
+    )
+
+csv_file_path = 'resources/Stanford_MSA_Database.csv'
+with open(csv_file_path, 'r') as file:
+    data_df = pd.read_csv(file)
+data_df.to_sql(con=engine, index_label='id', name='msa', if_exists='replace')
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+session = Session(engine)
+
+@app.route('/')
+def index():
+    #for row in session.query(Msa).all():
+    rows = engine.execute("SELECT * FROM msa").fetchall()
+    # for row in engine.table_names():
+    #      print(row)
+
+    # items = type(test)
+
+    return render_template("index.html", items=rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
