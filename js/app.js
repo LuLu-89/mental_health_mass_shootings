2ef20f2346327d9e0413280e3161c50605296e2e
LuLu-89 <lucy.ly89@gmail.com>
2019-06-29 12:18:43 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,22 @@
 var svgWidth = 960;
 var svgHeight = 660;
 
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
 var massShooting = d3.csv("/js/Stanford_MSA_Database.csv")
