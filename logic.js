73876d7b72828c60f8078fa4c11c54e84218f805
Faedra <47578942+Faedra@users.noreply.github.com>
2019-07-05 22:47:19 -0600

    updating map files and adding ipynb

--- logic.js
+++ logic.js
@@ -1,77 +1,14 @@
 
-// QUERY URL WITH EARTHQUAKE DATA
 // Start with a queryUrl from API and save as a variable
 var queryUrl = `https://raw.githubusercontent.com/StanfordGeospatialCenter/MSA/master/Data/Stanford_MSA_Database.geojson`;
 
-// USE RESULTS OF JSON TO MAP OUT ALL THE EARTHQUAKES FETCHED IN THE URL
 // use d3.json to pull in the data
-d3.json(queryUrl, function (data) {
-    console.log(data);
-    // create an L.geoJSON() ovelay later, and save the results to a variable (data.features is from our json)
-    // var shootings = L.geoJSON(data.features, {
-    //     // use onEachFeature function with standard geoJSON feature/layer arguments:
-    //     onEachFeature: function (feature, layer) {
-    //         // select a property from the json we want to display as popup; 
-    //         // here we choose property "place" which is written "feature.properties.place"
-    //         // and then wrapped in layer.bindPopup to make it the popup
-    //         // here we also use a javaScript "new Date" panel to display date info
-    //         layer.bindPopup(`<h3>${feature.properties.location}</h3>
-    //   <p>${feature.properties.date}</p>`);
-    //     }
-    
+d3.json(queryUrl, function (err, data) {
+    // console.log(data);
 
-    // // OUR STARTING DATA:
-    // var locations = [
-    //     {
-    //         properties: {
-    //             Title: "University of Texas at Austin",
-    //             Location: "Austin, Texas",
-    //             "Possible Motive - General": "Mental illness",
-    //             "History of Mental Illness - General": "Yes",
-    //             Date: "8/1/1966"
-    //         },
-    //         geometry: {
-    //             type: "Point",
-    //             coordinates: [-97.84415949, 30.1988873]
-    //         }
-    //     }
-    // ],
+    features = data.features;
 
-    // var locations = [
-    //     {
-    //         coordinates: [40.7128, -74.0059],
-    //         state: {
-    //             name: "New York State",
-    //             population: 19795791
-    //         },
-    //         city: {
-    //             name: "New York",
-    //             population: 8550405
-    //         }
-    //     },
-    //     {
-    //         coordinates: [34.0522, -118.2437],
-    //         state: {
-    //             name: "California",
-    //             population: 39250017
-    //         },
-    //         city: {
-    //             name: "Lost Angeles",
-    //             population: 3971883
-    //         }
-    //     },
-    // ];
-    features = data.features;//.slice(0,5);
-    /*.map(feature=>{
-        return {
-            coordinates : feature.geometry.coordinates,
-
-        }
-    })
-    */
-
-    // CREATE TILE LAYERS (SAVE TO VARIABLES):
-
+    // create tile layers
     var streets = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
@@ -79,73 +16,151 @@ d3.json(queryUrl, function (data) {
         accessToken: API_KEY
     });
 
-    //   var contrast = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    //     maxZoom: 18,
-    //     id: "mapbox.high-contrast",
-    //     accessToken: API_KEY
-    //   });
+    var contrast = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.high-contrast",
+        accessToken: API_KEY
+    });
 
-    // CREATE VARIABLES WITH ARRAYS WHERE WE'LL STORE OUR MARKERS
+    // create variables with arrays where we'll store our markers
+
     var shootingMarkers = [];
     for (var i = 0; i < features.length; i++) {
-        console.log("trigger");
-        //console.log(features[i].properties.Location)
+
         var popLocation = features[i].properties.Location;
         var popDate = features[i].properties.Date;
-        lat_long = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]
-        shootingMarkers.push(
-            
-            L.circle(lat_long, {
-                // stroke: true,
-                fillOpacity: 0.70,
-                color: "white",
-                weight: 1,
-                fillColor: "red",
-                radius: 80*1000
-            }).bindPopup(`<h2>${popLocation}</h2>
-            <h3>${popDate}</h3>`)
-        )};
-
-    var mentalMarkers = [];
-    
-    for (var i = 0; i < features.length; i++) {
-        // var population = locations[i].city.population;
-
+        var popTitle = features[i].properties.Title;
         lat_long = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]
         
+        shootingMarkers.push(
+
+            L.circle(lat_long, {
+                // stroke: true,
+                fillOpacity: 0.80,
+                color: "white",
+                weight: .8,
+                fillColor: "red",
+                radius: 80 * 1000
+            }).bindPopup(`
+            <h3>Shooting Location: ${popLocation}</h3>
+            <h3>Date: ${popDate}</h3>
+            <p><b>Description: </b>${popTitle}</h3>`)
+            )
+    };
+
+    var mentalMarkers = [];
+    for (var i = 0; i < features.length; i++) {
+
+        var popLocation = features[i].properties.Location;
+        var popDate = features[i].properties.Date;
+        var popMotive = features[i].properties["Possible Motive - General"];
+        var popHistory = features[i].properties["History of Mental Illness - Detailed"];
+        lat_long = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]
+
         if (features[i].properties['History of Mental Illness - General'] == 'Yes') {
-            
+
             mentalMarkers.push(
                 L.circle(lat_long, {
-                    fillOpacity: 0.75,
+                    fillOpacity: 0.9,
                     color: "white",
-                    weight: 1,
-                    fillColor: "orange",
-                    radius: 70*1000
-                })
-                );
+                    weight: .8,
+                    fillColor: "rgb(66, 185, 245)",
+                    radius: 80 * 1000
+                }).bindPopup(`
+                <h2 class="mental">History of Mental Illness: Yes</h2>
+                <h3>Location: ${popLocation}</h3>
+                <h3>Date: ${popDate}</h3>
+                <p><b>Mental Illness - General Motive:</b> ${popMotive}</p>
+                <p><b>History of Mental Illness:</b> ${popHistory}</p>`)
+            );
         };
     };
+
+    var schoolMarkers = [];
+    for (var i = 0; i < features.length; i++) {
+
+        var popLocation = features[i].properties.Location;
+        var popDate = features[i].properties.Date;
+        var popType = features[i].properties["Place Type"];
+        var popRelationship = features[i].properties["Relationship to Incident Location"];
+        var popTarget = features[i].properties["Targeted Victim/s - Detailed"];
+
+        lat_long = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]
+
+        if (features[i].properties['School Related'] == 'Yes') {
+
+            schoolMarkers.push(
+                L.circle(lat_long, {
+                    fillOpacity: 0.9,
+                    color: "white",
+                    weight: .8,
+                    fillColor: "rgb(56, 207, 106)",
+                    radius: 80 * 1000
+                }).bindPopup(`
+                <h2 class="school">School-Related: Yes</h2>
+                <h3>Location: ${popLocation}</h3>
+                <h3>Date: ${popDate}</h3>
+                <p><b>Type of School:</b> ${popType}</p>
+                <p><b>Shooter Relationship to Location:</b> ${popRelationship}</p>
+                <p><b>Targeted Victims:</b> ${popTarget}</p>`)
+            );
+        };
+    };
+
+    // var combinedMarkers = [];
+
+    // for (var i = 0; i < features.length; i++) {
+    //     // var population = locations[i].city.population;
+    //     var popLocation = features[i].properties.Location;
+    //     var popDate = features[i].properties.Date;
+    //     var popType = features[i].properties["Place Type"];
+    //     var popRelationship = features[i].properties["Relationship to Incident Location"];
+    //     var popTarget = features[i].properties["Targeted Victim/s - Detailed"];
+
+    //     lat_long = [features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]]
+
+    //     if (features[i].properties[
+    //         ['School Related'] == 'Yes'
+    //         ['History of Mental Illness - General'] == 'Yes'
+    //     ]) {
+
+    //         combinedMarkers.push(
+    //             L.circle(lat_long, {
+    //                 fillOpacity: 0.8,
+    //                 color: "white",
+    //                 weight: .8,
+    //                 fillColor: "orange",
+    //                 radius: 80 * 1000
+    //             }).bindPopup(`<h3>Location: ${popLocation}</h3>
+    //             <h3>Date: ${popDate}</h3><h3>Type of School: ${popType}</h3>
+    //             <p>Shooter Relationship to Location: ${popRelationship}</p><p>Targeted Victims: ${popTarget}</p>`)
+    //         );
+    //     };
+    // };
 
     // SET OUR BASE AND OVERLAY LAYERS:
     // make layer groups for city layer and state layer
     var shootingLayer = L.layerGroup(shootingMarkers);
     var mentalLayer = L.layerGroup(mentalMarkers);
+    var schoolLayer = L.layerGroup(schoolMarkers);
+    // var combinedLayer = L.layerGroup(combinedMarkers);
 
-    
-    // Set basemap with streetmap and darkmap map-ids
+
+    // Set basemap with streetmap and contrast map-ids
     var baseMaps = {
-        // "Contrast": contrast,
-        // "Street": streets
+        "Contrast": contrast,
+        "Street": streets
     };
-    // Create an overlayMaps object here to contain the "State Population" and "City Population" layers
+    // Create an overlayMaps object here to contain the various layers
     var overlayMaps = {
-        "Shooting Locations": shootingLayer,
-        "Mental Health Motive": mentalLayer
+        "Shootings: 335 Locations": shootingLayer,
+        "History of Mental Illness: 96 Locations": mentalLayer,
+        "School Related: 74 Locations": schoolLayer,
+        // "Mental-School Combined": combinedLayer,
     }
 
-    // CREATE OUR MAP OBJECT "L.MAP" AND DEFAULT LAYERS
+    // create our map object and default layers
     var map = L.map("map", {
         center: [37.09, -95.71],
         zoom: 4,
@@ -153,5 +168,7 @@ d3.json(queryUrl, function (data) {
     });
 
     // Create a layer control, containing our baseMaps and overlayMaps, and add them to the map
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
+    L.control
+    .layers(baseMaps, overlayMaps, { collapsed:false })
+    .addTo(map);
 });
